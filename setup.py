b795e9aef7d1b9c3665f5be2f175ef065e9471f1
Matt Daye <mdaye13@gmail.com>
2016-06-24 16:13:07 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='fancystats',
+      version='0.1',
+      description='Making your stats fancier',
+      url='http://github.com/firstlinestats/fancystats',
+      author='First Line Stats',
+      author_email='firstlinestats@gmail.com',
+      license='MIT',
+      packages=['fancystats'],
+      zip_safe=False)
