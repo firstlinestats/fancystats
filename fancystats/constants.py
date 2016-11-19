1945a4ae18ac38246f433e0713d3c2add0bed920
Matt Daye <mdaye13@gmail.com>
2016-11-19 12:37:55 -0500

    adding more constants

--- fancystats/constants.py
+++ fancystats/constants.py
@@ -1,5 +1,36 @@
 
 
+TEAMSTRENGTHS_CHOICES = (
+    ("all", "All"),
+    ("even", "Even Strength 5v5"),
+    ("pp", "Power Play"),
+    ("pk", "Short Handed"),
+    ("4v4", "4v4"),
+    ("og", "Opposing Goalie Pulled"),
+    ("tg", "Team Goalie Pulled"),
+    ("3v3", "3v3")
+)
+
+SCORESITUATION_CHOICES = (
+    ("all", "All"),
+    ("t3+", "Trailing by 3+"),
+    ("t2", "Trailing by 2"),
+    ("t1", "Trailing by 1"),
+    ("t", "Tied"),
+    ("l1", "Leading by 1"),
+    ("l2", "Leading by 2"),
+    ("l3+", "Leading by 3+"),
+    ("w1", "Within 1")
+)
+
+PERIOD_CHOICES = (
+    ("all", "All"),
+    ("1", "1"),
+    ("2", "2"),
+    ("3", "3"),
+    ("4", "OT")
+)
+
 gameTypes = (
     ("PR", "Pre-season"),
     ("R", "Regular season"),
